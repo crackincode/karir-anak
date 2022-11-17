c1a2f9cc377b05b91ab8908a23cda78cb1becedb
Crackincode <105475401+crackincode@users.noreply.github.com>
2022-11-17 13:59:19 +0700

    update kodingan

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -28,7 +28,7 @@ const count = ref(0)
   <p>
     Install
     <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
+    for Vue 3 TypeScript support.
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
